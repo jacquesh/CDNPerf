57b54c436270d3dbf03b44d1c5116aec8cf58b5d
Brian Mc George <Brian-McG.github@outlook.com>
2016-08-28 17:49:41 +0200

    Fix an issue with detection of localIP in Windows

--- findip.py
+++ findip.py
@@ -10,7 +10,7 @@ from time import sleep
 from datetime import datetime
 
 # Set this to subprocess.DEVNULL for clean output, None for verbose output
-verboseOutputTarget = subprocess.DEVNULL
+verboseOutputTarget = None
 
 # Retry attemps
 numberOfRetries = 3
@@ -60,11 +60,12 @@ def getLocalIP():
         ipconfigLines = subprocess.Popen(['ipconfig'], stdout=subprocess.PIPE, stderr=verboseOutputTarget).stdout.readlines()
         localIP = '(Unknown)'
         for line in ipconfigLines:
-            lineStr = str(line.strip())
-            if not lineStr.startswith('IPv4 Address'):
+            lineStr = str(line.decode().strip())
+            index = lineStr.find('IPv4 Address')
+            if index == -1:
                 continue
             localIP = lineStr[lineStr.find(':') + 2:]
-        return localIP
+        return localIP.strip()
     else:
         ipRoute = subprocess.Popen(["ip", "route", "get", "8.8.8.8"], stdout=subprocess.PIPE)
         return subprocess.check_output(["awk", "{print $NF; exit}"], stdin=ipRoute.stdout).strip().decode()
